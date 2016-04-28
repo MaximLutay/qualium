e7b65a0c9e821a45f3731f54e9cd910669387d1a
Maxim Lutay <maksym.lutai@nure.ua>
2016-04-28 17:03:58 +0300

    Update body_backgr.js

--- source/body_backgr.js
+++ source/body_backgr.js
@@ -1,6 +1,6 @@
 var anchor = document.getElementById('anchor');
 
-var body = document.getElementsByTagName('body')[0];
+var body = function(){return document.getElementsByTagName('body')[0];};
 var bodywidth = body.style.width;
 
 
@@ -13,4 +13,4 @@ var link_0 = document.getElementsByTagName('a')[0];
 anchor.addEventListener('mouseover',function(){body.style.background = "white";})
 anchor.addEventListener('mouseout',function(){body.style.background = "gray";})
 
-body.addEventListener('resize',function(){link_0.style.left = bodywidth/2 + link_0.style.width + 'px';})
+body.addEventListener('resize',function(){link_0.style.left = bodywidth()/2 + link_0.style.width + 'px';})
