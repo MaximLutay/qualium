bd89becd88ccb6ced6b87ea3a5e92251cb8bf100
Maxim Lutay <maksym.lutai@nure.ua>
2016-04-28 16:46:58 +0300

    Create body_backgr.js

---
+++ source/body_backgr.js
@@ -0,0 +1,7 @@
+var anchor = document.getElementById('#anchor');
+
+var body = document.getElementsByTagName('body')[0];
+
+
+
+anchor.addEventListener('mouseover',function(){body.style.background = "white";})
