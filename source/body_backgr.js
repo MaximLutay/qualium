dada3ccedd041e179f3bb7b55ab54b4590b4660d
Maxim Lutay <maksym.lutai@nure.ua>
2016-04-28 16:53:09 +0300

    Update body_backgr.js

--- source/body_backgr.js
+++ source/body_backgr.js
@@ -5,3 +5,4 @@ var body = document.getElementsByTagName('body')[0];
 
 
 anchor.addEventListener('mouseover',function(){body.style.background = "white";})
+anchor.addEventListener('mouseout',function(){body.style.background = "gray";})
